bccb7018b575c12a97496da816fab3a3554e219f
vava24680 <9203700@gmail.com>
2017-04-21 12:49:57 +0800

    temporary code added in

---
+++ algo_hw2.hpp
@@ -0,0 +1,59 @@
+
+// change this to your id
+static const char* student_id = "3345678" ;
+
+
+// do not edit prototype
+void Insert(int *, int);
+void Delete(int *, int);
+int Select(int *, int);
+int Rank(int *, int);
+
+// data structure : 
+// tree is an array with enough space
+// tree[0] is the array size
+// bundle three attribute as a node data
+// First attribute is color, "0" means black, "1" means red , NIL is also "0", "-1" means no data
+// Second attribute is key, "0" means NIL, "1"~"999" means data,  "-1" means no data
+// Third attribute is size, for Dynamic Order Statistics, "0" means NIL , "-1" means no data
+//
+// for example,
+// if tree[0] is "256" says that the array size is 256
+//
+// if tree[1] is "1" says that the place of 1 is a red node
+//
+// if tree[2] is "5" says that the place of 1 is key of 5
+//
+// if tree[8] is "-1" says that the place of 3 has nothing
+//
+// if tree[14] is "-1" says that the place of 5 is a node of NIL
+//
+// if tree[19] is "66" says that the place of 7 is key of 66
+
+
+//
+// do your homework here
+//
+
+void Insert(int * tree, int key) {
+
+}
+
+void Delete(int * tree, int key) {
+	//
+	// if there is ambiguous situation, choose the smaller or left one
+	//
+
+}
+
+int Select(int * tree, int i) {
+	// use Dynamic Order Statistics to tell me the i'th smallest element
+	int output_key;
+	return output_key;
+}
+
+int Rank(int * tree, int x) {
+	// use Dynamic Order Statistics to tell me the rank of element x in the tree
+	int output_rank;
+	return output_rank;
+}
